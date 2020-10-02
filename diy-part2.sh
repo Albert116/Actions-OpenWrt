f0a1a6906e0128fc65725008c633c8f212481a9c
Albert <albertforwork@foxmail.com>
2020-10-02 16:20:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-git clone https://github.com/fw876/helloworld.git package/lean/luci-app-helloworld
-sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+
+#git clone https://github.com/fw876/helloworld.git package/lean/luci-app-helloworld
+#sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
